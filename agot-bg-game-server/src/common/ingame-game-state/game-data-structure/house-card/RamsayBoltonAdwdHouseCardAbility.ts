10de1103f05aa53a24877729b38630b3972d7c21
andisichert <andisichert@gmail.com>
2021-01-24 11:41:53 +0100

    ramsay

--- agot-bg-game-server/src/common/ingame-game-state/game-data-structure/house-card/RamsayBoltonAdwdHouseCardAbility.ts
+++ agot-bg-game-server/src/common/ingame-game-state/game-data-structure/house-card/RamsayBoltonAdwdHouseCardAbility.ts
@@ -4,16 +4,23 @@ import House from "../House";
 import CombatGameState from "../../action-game-state/resolve-march-order-game-state/combat-game-state/CombatGameState";
 
 export default class RamsayBoltonAdwdHouseCardAbility extends HouseCardAbility {
+    modifySwordIcons(_combat: CombatGameState, _house: House, _houseCard: HouseCard, _affectedHouseCard: HouseCard): number {
+        return this.doesTrigger(_houseCard, _affectedHouseCard, _house) ? 3 : 0;
+    }
 
     modifyHouseCardCombatStrength(_combat: CombatGameState, _house: House, _houseCard: HouseCard, _affectedHouseCard: HouseCard): number {
-        var discardedCards = 0;
+        return this.doesTrigger(_houseCard, _affectedHouseCard, _house) ? 1 : 0;
+    }
 
-        _house.houseCards.forEach((card, _) =>{
-        if (card.state == HouseCardState.USED) {
-            discardedCards += 1;
-            }
-        });
+    doesTrigger(houseCard: HouseCard, affectedHouseCard: HouseCard, house: House): boolean {
+        if (!house.houseCards.has("reek-adwd")) {
+            // This should never happen as Stannis Baratheon will always be with Ser Davos
+            // Seaworth. It could happen if a house is created with Davos but without Stannis.
+            return false;
+        }
 
-        return _houseCard == _affectedHouseCard ? discardedCards : 0;
+        const reekAdwd = house.houseCards.get("reek-adwd");
+
+        return houseCard == affectedHouseCard && reekAdwd.state == HouseCardState.AVAILABLE;
     }
 }
